049be22ecf9994c597b977dfb50947c85a117393
CodeSandbox <support@codesandbox.io>
2023-01-26 21:30:41 +0000

    "Initial commit"

---
+++ intro-to-express/calculator.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const App = express();
+App.use(bodyParser.urlencoded({ extended: true }));
+App.get("/", (req, res) => res.sendFile(__dirname + "/calculator.html"));
+App.post("/", (req, res) => {
+  var n1 = Number(req.body.n1);
+  var n2 = Number(req.body.n2);
+  var result = n1 + n2;
+  res.send("The answer is " + result);
+});
+
+App.listen(3000, () =>
+  console.log("The server has started running on port 3000")
+);
